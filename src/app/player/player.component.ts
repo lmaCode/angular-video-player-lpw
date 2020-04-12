d35702c908637ae2097e9d3fe6992cfd99ae66bd
lmaCode <lipengwei_seu@163.com>
2020-04-12 12:45:16 +0800

    2 files modified

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
-
+  src;
+  id;
+  poster;
+  onended;
   constructor() { }
 
   ngOnInit() {
-  }
+    this.src="assets/video.mp4";
+    this.id="video1";
+    this.poster="assets/cover.jpg";
+    this.onended="afterend()";
+   }
+
+   afterend(){
+     
+   }
 
 }
